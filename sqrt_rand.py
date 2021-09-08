59eb159ed660a8c6ad7dee769c91734f734bcf4d
Angel Fernandez <21150449+AngelFerndz@users.noreply.github.com>
2021-09-08 18:34:13 -0400

    Fixed bug breaking the rng

--- sqrt_rand.py
+++ sqrt_rand.py
@@ -6,32 +6,40 @@ class sqrt_rand:
         self.seed = seed
         self.increment = increment
         
-    def rand(self, value=1, positive=True, negative=True, increase=True):
+    def rand(self, value=1, increase=True):
         x = self.sqrt(self.seed) 
         y = x - round(x)
         result = y * value * 2
-        
+ 
         if increase:
-            self.increase_increment()
+            self.increase_seed()    
         
+        return result
+    
+    def modify(self, result, positive=True, negative=True):
         if positive and negative == False and result < 0:
             result = -result
         elif negative and positive == False and result > 0:
-            result = -result
+            result = -result    
         return result
-    
-    def randp(self, value=1, increase=True):
-        return self.rand(value, positive=True, negative=False, increase=increase)
-    
-    def randn(self, value=1, increase=True):
-        return self.rand(value, positive=False, negative=True, increase=increase)
         
-    def increase_increment(self, increment=1):
+    def next_rand(self, value=1, positive=True, negative=True):
+        self.randomize()
+        return self.modify(self.rand(value), positive, negative)
+    
+    def randp(self, value=1):
+       self.randomize()
+       return self.modify(self.rand(value), True, False)
+    
+    def randn(self, value=1):
+        self.randomize()
+        return self.modify(self.rand(value), False, True)
+        
+    def increase_seed(self, increment=1):
         self.seed = self.seed + increment
         
-    def rand_increment(self, value=1):
-        self.increment = self.rand(value)
+    def rand_seed(self, value=1): 
+        self.seed =  self.modify(self.rand(value), True, False)
         
-    def rand_seed(self, value=1):
-        self.seed = self.randp(value)
-        
+    def randomize(self):
+        self.rand_seed(self.increment)
